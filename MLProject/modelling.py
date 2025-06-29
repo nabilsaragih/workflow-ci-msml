1492515bbc523c55cd50c1708fb2499006de111f
nabilsaragih <nabilsaragih33@gmail.com>
2025-06-30 01:24:23 +0800

    add: workflow

---
+++ MLProject/modelling.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestClassifier
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_path", type=str, default="personality_preprocessing", help="Path ke folder dataset")
+args = parser.parse_args()
+
+X_train = pd.read_csv(os.path.join(args.data_path, "X_train_scaled.csv"))
+y_train = pd.read_csv(os.path.join(args.data_path, "y_train.csv")).values.ravel()
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("Random Forest Autolog Basic")
+mlflow.sklearn.autolog()
+
+with mlflow.start_run():
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    print("Model dilatih dan dicatat dengan autolog.")
